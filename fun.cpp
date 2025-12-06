b6ccb36373f32cd8e103a7c2ae1120105115eeca
Rishu raj <21je0769@iitism.ac.in>
2025-12-06 12:47:31 +0530

    added fun.cpp and power.cpp to find power

---
+++ fun.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{cout<<"hwllo world"<<endl;
+
+    return 0;
+}
---
+++ power.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int power(int x, int y, int m)
+{
+    int result = 1;
+    x = x % m;
+    while (y > 0)
+    {
+        if (y & 1)
+            result = (result * x) % m;
+        y = y >> 1;
+        x = (x * x) % m;
+    }
+    return result;
+}
+int modInverse(int Q, int m)
+{
+    return power(Q, m - 2, m);
+}
+int32_t main()
+{
+    cout<<power(2,3,1e9+7)<<endl;
+    return 0;
+}
+// (2**3)%1e9+7
